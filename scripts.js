fefa5059f54feebb39df7559af15ac729f591b8b
iain-moss <48953988+iain-moss@users.noreply.github.com>
2019-11-07 14:39:52 +0000

    Send to London Team

--- scripts.js
+++ scripts.js
@@ -11,6 +11,18 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+    $("#resources-button").click(function() {
+        $(this).find("#resources-show").slideToggle();
+    });
+    $(document).on("click", function(event) {
+        var $trigger = $("#resources-button");
+        if ($trigger !== event.target && !$trigger.has(event.target).length) {
+            $("#resources-show").slideUp();
+        };
+    })
+});
+
+$(document).ready(function() {
     $("#apply-button").click(function() {
         $("#apply-button").hide();
         $("div.application-procedures").slideToggle();
